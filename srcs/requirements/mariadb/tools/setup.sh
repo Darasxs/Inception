e5a69a2aa7b7e5ac61bcc53baf74bcc2396cbf34
Dariusz Paluszkiewicz <dpaluszk@student.42heilbronn.de>
2026-03-08 00:26:42 +0100

    MariaDB container fixed and working

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
+set -e
 
-service mariadb start
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Initializing MariaDB data directory..."
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+fi
 
-mysql -u root <<EOF
-CREATE DATABASE ${MYSQL_DATABASE};
-CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+echo "Starting MariaDB temporarily..."
+mysqld_safe --datadir=/var/lib/mysql &
+
+echo "Waiting for MariaDB to be ready..."
+until mysqladmin ping --silent; do
+    sleep 1
+done
+
+echo "Configuring database..."
+mysql <<EOF
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
 
-mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+echo "Shutting down temporary MariaDB..."
+mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
 
-exec mysqld_safe
+echo "Starting MariaDB in foreground..."
+exec mysqld_safe --datadir=/var/lib/mysql
