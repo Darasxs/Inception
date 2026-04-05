c1b7a32e35ae87cfc9b3af5970e620dfd2769d95
Dariusz Paluszkiewicz <dpaluszk@student.42heilbronn.de>
2026-04-05 14:42:50 +0200

    setup script for wordpress created, also files and directories for nginx created

---
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+
+mkdir -p /var/www/html
+cd /var/www/html
+
+echo "Waiting for MariaDB..."
+until mysqladmin ping -h mariadb -u${MYSQL_USER} -p${MYSQL_PASSWORD} --silent; do
+    sleep 1
+done
+
+if [ ! -f wp-config.php ]; then
+    echo "Downloading WordPress..."
+
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
+
+    wp core download --allow-root
+
+    echo "Configuring WordPress..."
+
+    wp config create \
+        --dbname=${MYSQL_DATABASE} \
+        --dbuser=${MYSQL_USER} \
+        --dbpass=${MYSQL_PASSWORD} \
+        --dbhost=mariadb \
+        --allow-root
+
+    wp core install \
+        --url=${DOMAIN_NAME} \
+        --title="${WP_TITLE}" \
+        --admin_user=${WP_ADMIN_USER} \
+        --admin_password=${WP_ADMIN_PASSWORD} \
+        --admin_email=${WP_ADMIN_EMAIL} \
+        --allow-root
+
+    wp user create \
+        ${WP_USER} ${WP_USER_EMAIL} \
+        --user_pass=${WP_USER_PASSWORD} \
+        --allow-root
+fi
+
+chown -R www-data:www-data /var/www/html
+
+echo "Starting PHP-FPM..."
+exec php-fpm8.2 -F
